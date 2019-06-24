5f4845ead26fd657ca4dbc101c650dddaeeca904
Leonardo Rogrigues Moreira <leochuckdk@gmail.com>
2019-06-24 09:20:57 -0300

    Commit Inicial

---
+++ android/app/src/main/java/com/liderautomacao/telalog/MainActivity.java
@@ -0,0 +1,13 @@
+package com.liderautomacao.telalog;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
